d428a4978b34b8eb0583e02d03cd2af9fb30e7f2
Mike Interlandi <43190101+Minterl@users.noreply.github.com>
2026-04-24 00:49:23 -0400

    refactor the move union

--- src/parser.rs
+++ src/parser.rs
@@ -1,4 +1,4 @@
-use crate::cube::Move;
+use crate::cube::{Move, Direction};
 use anyhow::{anyhow, Error};
 
 const DELIMITER: char = ' ';
@@ -12,15 +12,33 @@ pub fn parse(moves: &str) -> Result<Vec<Move>, Error> {
         .split(DELIMITER)
         .filter(|s| !s.is_empty())
         .map(|s| match s {
-            "U" | "u" => Ok(Move::U),
-            "D" | "d" => Ok(Move::D),
-            "F" | "f" => Ok(Move::F),
-            "B" | "b" => Ok(Move::B),
-            "L" | "l" => Ok(Move::L),
-            "R" | "r" => Ok(Move::R),
-            "M" | "m" => Ok(Move::M),
-            "E" | "e" => Ok(Move::E),
-            "S" | "s" => Ok(Move::S),
+            "U" | "u" => Ok(Move::U(Direction::Clockwise)),
+            "U'" | "u'" => Ok(Move::U(Direction::Prime)),
+            "U2" | "u2" => Ok(Move::U(Direction::Double)),
+            "D" | "d" => Ok(Move::D(Direction::Clockwise)),
+            "D'" | "d'" => Ok(Move::D(Direction::Prime)),
+            "D2" | "d2" => Ok(Move::D(Direction::Double)),
+            "F" | "f" => Ok(Move::F(Direction::Clockwise)),
+            "F'" | "f'" => Ok(Move::F(Direction::Prime)),
+            "F2" | "f2" => Ok(Move::F(Direction::Double)),
+            "B" | "b" => Ok(Move::B(Direction::Clockwise)),
+            "B'" | "b'" => Ok(Move::B(Direction::Prime)),
+            "B2" | "b2" => Ok(Move::B(Direction::Double)),
+            "L" | "l" => Ok(Move::L(Direction::Clockwise)),
+            "L'" | "l'" => Ok(Move::L(Direction::Prime)),
+            "L2" | "l2" => Ok(Move::L(Direction::Double)),
+            "R" | "r" => Ok(Move::R(Direction::Clockwise)),
+            "R'" | "r'" => Ok(Move::R(Direction::Prime)),
+            "R2" | "r2" => Ok(Move::R(Direction::Double)),
+            "M" | "m" => Ok(Move::M(Direction::Clockwise)),
+            "M'" | "m'" => Ok(Move::M(Direction::Prime)),
+            "M2" | "m2" => Ok(Move::M(Direction::Double)),
+            "E" | "e" => Ok(Move::E(Direction::Clockwise)),
+            "E'" | "e'" => Ok(Move::E(Direction::Prime)),
+            "E2" | "e2" => Ok(Move::E(Direction::Double)),
+            "S" | "s" => Ok(Move::S(Direction::Clockwise)),
+            "S'" | "s'" => Ok(Move::S(Direction::Prime)),
+            "S2" | "s2" => Ok(Move::S(Direction::Double)),
             "X" | "x" | "Y" | "y" | "Z" | "z" => Err(anyhow!("attempted to rotate cube (unsupported)")),
             other => Err(anyhow!("not a valid WCA move: {other}"))
         })
@@ -43,7 +61,7 @@ mod test {
     fn test_multiple_moves() {
         let result = parse("U R F");
         assert!(result.is_ok());
-        assert_eq!(result.unwrap(), vec![Move::U, Move::R, Move::F]);
+        assert_eq!(result.unwrap(), vec![Move::U(Direction::Clockwise), Move::R(Direction::Clockwise), Move::F(Direction::Clockwise)]);
     }
 
     #[test]
