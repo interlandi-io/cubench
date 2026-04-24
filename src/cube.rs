103fcd40e23e95748124da41603863a3e7283eaa
Mike Interlandi <43190101+Minterl@users.noreply.github.com>
2026-04-24 11:41:11 -0400

    use traits for move codec

--- src/cube.rs
+++ src/cube.rs
@@ -1,4 +1,5 @@
 use std::{fmt::{Display, Formatter}, time::{SystemTime, UNIX_EPOCH}};
+use anyhow::{anyhow, Error};
 
 const KNUTH: u64 = 0x9E37_79B9_7F4A_7C15;
 
@@ -404,6 +405,44 @@ impl Display for Move {
         f.write_str(s)?;
         
         Ok(())
+    }
+}
+
+impl TryFrom<&str> for Move {
+    type Error = Error;
+    
+    fn try_from(value: &str) -> Result<Self, Self::Error> {
+       match value {
+            "U" | "u" => Ok(Move::U(Direction::Clockwise)),
+            "U'" | "u'" => Ok(Move::U(Direction::Prime)),
+            "U2" | "u2" => Ok(Move::U(Direction::Double)),
+            "D" | "d" => Ok(Move::D(Direction::Clockwise)),
+            "D'" | "d'" => Ok(Move::D(Direction::Prime)),
+            "D2" | "d2" => Ok(Move::D(Direction::Double)),
+            "F" | "f" => Ok(Move::F(Direction::Clockwise)),
+            "F'" | "f'" => Ok(Move::F(Direction::Prime)),
+            "F2" | "f2" => Ok(Move::F(Direction::Double)),
+            "B" | "b" => Ok(Move::B(Direction::Clockwise)),
+            "B'" | "b'" => Ok(Move::B(Direction::Prime)),
+            "B2" | "b2" => Ok(Move::B(Direction::Double)),
+            "L" | "l" => Ok(Move::L(Direction::Clockwise)),
+            "L'" | "l'" => Ok(Move::L(Direction::Prime)),
+            "L2" | "l2" => Ok(Move::L(Direction::Double)),
+            "R" | "r" => Ok(Move::R(Direction::Clockwise)),
+            "R'" | "r'" => Ok(Move::R(Direction::Prime)),
+            "R2" | "r2" => Ok(Move::R(Direction::Double)),
+            "M" | "m" => Ok(Move::M(Direction::Clockwise)),
+            "M'" | "m'" => Ok(Move::M(Direction::Prime)),
+            "M2" | "m2" => Ok(Move::M(Direction::Double)),
+            "E" | "e" => Ok(Move::E(Direction::Clockwise)),
+            "E'" | "e'" => Ok(Move::E(Direction::Prime)),
+            "E2" | "e2" => Ok(Move::E(Direction::Double)),
+            "S" | "s" => Ok(Move::S(Direction::Clockwise)),
+            "S'" | "s'" => Ok(Move::S(Direction::Prime)),
+            "S2" | "s2" => Ok(Move::S(Direction::Double)),
+            "X" | "x" | "Y" | "y" | "Z" | "z" => Err(anyhow!("attempted to rotate cube (unsupported)")),
+            other => Err(anyhow!("not a valid WCA move: {other}"))
+       }
     }
 }
 
--- src/parser.rs
+++ src/parser.rs
@@ -1,4 +1,4 @@
-use crate::cube::{Move, Direction};
+use crate::cube::Move;
 use anyhow::{anyhow, Error};
 
 const DELIMITER: char = ' ';
@@ -11,43 +11,14 @@ pub fn parse(moves: &str) -> Result<Vec<Move>, Error> {
         .trim()
         .split(DELIMITER)
         .filter(|s| !s.is_empty())
-        .map(|s| match s {
-            "U" | "u" => Ok(Move::U(Direction::Clockwise)),
-            "U'" | "u'" => Ok(Move::U(Direction::Prime)),
-            "U2" | "u2" => Ok(Move::U(Direction::Double)),
-            "D" | "d" => Ok(Move::D(Direction::Clockwise)),
-            "D'" | "d'" => Ok(Move::D(Direction::Prime)),
-            "D2" | "d2" => Ok(Move::D(Direction::Double)),
-            "F" | "f" => Ok(Move::F(Direction::Clockwise)),
-            "F'" | "f'" => Ok(Move::F(Direction::Prime)),
-            "F2" | "f2" => Ok(Move::F(Direction::Double)),
-            "B" | "b" => Ok(Move::B(Direction::Clockwise)),
-            "B'" | "b'" => Ok(Move::B(Direction::Prime)),
-            "B2" | "b2" => Ok(Move::B(Direction::Double)),
-            "L" | "l" => Ok(Move::L(Direction::Clockwise)),
-            "L'" | "l'" => Ok(Move::L(Direction::Prime)),
-            "L2" | "l2" => Ok(Move::L(Direction::Double)),
-            "R" | "r" => Ok(Move::R(Direction::Clockwise)),
-            "R'" | "r'" => Ok(Move::R(Direction::Prime)),
-            "R2" | "r2" => Ok(Move::R(Direction::Double)),
-            "M" | "m" => Ok(Move::M(Direction::Clockwise)),
-            "M'" | "m'" => Ok(Move::M(Direction::Prime)),
-            "M2" | "m2" => Ok(Move::M(Direction::Double)),
-            "E" | "e" => Ok(Move::E(Direction::Clockwise)),
-            "E'" | "e'" => Ok(Move::E(Direction::Prime)),
-            "E2" | "e2" => Ok(Move::E(Direction::Double)),
-            "S" | "s" => Ok(Move::S(Direction::Clockwise)),
-            "S'" | "s'" => Ok(Move::S(Direction::Prime)),
-            "S2" | "s2" => Ok(Move::S(Direction::Double)),
-            "X" | "x" | "Y" | "y" | "Z" | "z" => Err(anyhow!("attempted to rotate cube (unsupported)")),
-            other => Err(anyhow!("not a valid WCA move: {other}"))
-        })
+        .map(|s| Move::try_from(s))
         .collect()
 }
 
 #[cfg(test)]
 mod test {
     use super::*;
+    use crate::cube::Direction;
 
     #[test]
     fn test_valid_single_moves() {
