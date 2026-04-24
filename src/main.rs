4be6060b431b050007d784f68b54d50da30d9e96
Mike Interlandi <43190101+Minterl@users.noreply.github.com>
2026-04-24 11:32:21 -0400

    whoops

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,26 @@
-fn main() {
-    println!("Hello, world!");
+use cubench::cube::Cube;
+use cubench::parser::parse;
+use anyhow::Error;
+use std::io;
+
+fn main() -> Result<(), Error> {
+    let mut cube = Cube::new_solved();
+    cube.scramble(128);
+    let mut solution_buf = String::new();
+
+    println!("{}", cube.to_string());
+
+    println!("Enter solution");
+    io::stdin().read_line(&mut solution_buf)?;
+
+    let solution_moves = parse(&solution_buf)?;
+
+    for r#move in solution_moves {
+        cube.r#move(r#move);
+    }
+
+    println!("Cube state: {cube}");
+    println!("Solved: {}", cube.is_solved());
+
+    Ok(())
 }
