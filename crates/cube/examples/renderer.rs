e411f27e473fd8fe4f1d4afd646c03ed15f974b2
Mike Interlandi <43190101+Minterl@users.noreply.github.com>
2026-04-25 08:42:44 -0400

    move logic to renderer

--- crates/cube/examples/renderer.rs
+++ crates/cube/examples/renderer.rs
@@ -1,44 +1,13 @@
-use cubench::renderer::Renderer;
+use cubench::renderer::{Renderer, DrawResult};
 use kiss3d::prelude::*;
 
 #[kiss3d::main]
 async fn main() -> Result<(), anyhow::Error> {
-    let mut window = Window::new("").await;
-    let mut camera = OrbitCamera3d::new(
-        Vec3 {
-            x: 15.0,
-            y: 15.0,
-            z: -15.0,
-        },
-        Vec3 {
-            x: 0.0,
-            y: 0.0,
-            z: 0.0,
-        },
-    );
-    let mut scene = SceneNode3d::empty();
-    scene
-        .add_light(Light::point(100.0))
-        .set_position(Vec3::new(0.0, 10.0, 0.0));
-
-    let mut cube = scene.add_group();
-    for i in 0..=2 {
-        for j in 0..=2 {
-            for k in 0..=2 {
-                if i == 1 && j == 1 && k == 1 {
-                    continue;
-                }
-                let x = (1 - i) as f32 * 1.1;
-                let y = (1 - j) as f32 * 1.1;
-                let z = (1 - k) as f32 * 1.1;
-                cube.add_cube(1.0, 1.0, 1.0).translate(Vec3 { x, y, z });
-            }
-        }
-    }
-
+    let mut renderer = Renderer::new().await;
     let rot = Quat::from_axis_angle(Vec3::Y, 0.014);
 
-    while window.render_3d(&mut scene, &mut camera).await {
-        cube.rotate(rot);
-    }
+    renderer.draw_loop(|scene| {
+        scene.cube.rotate(rot);
+        DrawResult::Continue
+    }).await;
 }
--- crates/cube/src/renderer.rs
+++ crates/cube/src/renderer.rs
@@ -1,13 +1,107 @@
+pub use image::{ImageBuffer, Rgb};
 use kiss3d::prelude::*;
+use std::sync::Arc;
 
-pub struct Renderer {}
+pub struct Renderer {
+    window: Window,
+    scene: Scene,
+}
+
+#[allow(unused)]
+pub struct Scene {
+    pub camera: OrbitCamera3d,
+    pub root: SceneNode3d,
+    pub light: SceneNode3d,
+    pub cube: SceneNode3d,
+    pub cubelets: Vec<Cubelet>,
+}
+
+pub struct Cubelet {
+    pub node: SceneNode3d,
+    pub texture: Arc<Texture>,
+}
+
+pub enum DrawResult {
+    Continue,
+    Break,
+}
 
 impl Renderer {
-    pub fn new() -> Self {
-        todo!()
+    pub async fn new() -> Self {
+        let window = Window::new("").await;
+        let camera = OrbitCamera3d::new(
+            Vec3 {
+                x: 15.0,
+                y: 15.0,
+                z: -15.0,
+            },
+            Vec3 {
+                x: 0.0,
+                y: 0.0,
+                z: 0.0,
+            },
+        );
+        let mut root = SceneNode3d::empty();
+
+        let mut cube = root.add_group();
+        let light = root 
+            .add_light(Light::point(100.0))
+            .set_position(Vec3::new(0.0, 10.0, 0.0));
+
+        let mut cubelets = Vec::with_capacity(26); 
+        for i in 0..=2 {
+            for j in 0..=2 {
+                for k in 0..=2 {
+                    if i == 1 && j == 1 && k == 1 {
+                        continue;
+                    }
+                    let x = (1 - i) as f32 * 1.1;
+                    let y = (1 - j) as f32 * 1.1;
+                    let z = (1 - k) as f32 * 1.1;
+                    let node = cube.add_cube(1.0, 1.0, 1.0)
+                        .translate(Vec3 { x, y, z });
+                    cubelets.push(Cubelet {
+                        node,
+                        texture: Texture::new_default(),
+                    });
+                }
+            }
+        }
+
+        let scene = Scene {
+            camera,
+            root,
+            light,
+            cube,
+            cubelets,
+        };
+
+        Self {
+            window,
+            scene,
+        }
     }
 
-    pub fn draw(&self) -> Result<(), anyhow::Error> {
-        todo!()
+    pub async fn draw_once(&mut self) {
+        self.window.render_3d(
+            &mut self.scene.root,
+            &mut self.scene.camera
+        ).await;
+    }
+
+    pub async fn draw_loop(&mut self, mut f: impl FnMut(&mut Scene) -> DrawResult) {
+        while self.window.render_3d(
+            &mut self.scene.root,
+            &mut self.scene.camera
+        ).await {
+            match f(&mut self.scene) {
+                DrawResult::Continue => (),
+                DrawResult::Break => return,
+            }
+        }
+    }
+
+    pub fn render_to_buf(&self) -> ImageBuffer<Rgb<u8>, Vec<u8>> {
+        self.window.snap_image()
     }
 }
