c0f08801dde01256ca837acc627d0516e0a4863c
Mike Interlandi <43190101+Minterl@users.noreply.github.com>
2026-04-24 20:18:51 -0400

    we can see a cube

--- crates/cube/src/lib.rs
+++ crates/cube/src/lib.rs
@@ -1,3 +1,4 @@
 pub mod cube;
 pub mod cube_shim;
 pub mod parser;
+pub mod renderer;
---
+++ crates/cube/src/renderer.rs
@@ -0,0 +1,14 @@
+use kiss3d::prelude::*;
+
+pub struct Renderer {
+}
+
+impl Renderer {
+    pub fn new() -> Self {
+        todo!()
+    }
+
+    pub fn draw(&self) -> Result<(), anyhow::Error> {
+        todo!()
+    }
+}
